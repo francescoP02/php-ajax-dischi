7573f49ecc4c59d93359c3c728bebdd95aa1d9f4
francescoP02 <file.partipilo@gmail.com>
2022-06-10 16:47:41 +0200

    Added MILESTONE 1

--- api/index-json.php
+++ api/index-json.php
@@ -4,12 +4,21 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Vue Dischi</title>
+
+  <!-- AXIOS -->
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.27.2/axios.min.js" integrity="sha512-odNmoc1XJy5x1TMVMdC7EMs3IVdItLPlCeL5vSUPN2llYKMJ2eByTTAIiiuqLg+GdNr9hF6z81p27DArRFKT7A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+  <!-- VUEJS -->
+
+  <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+
 </head>
 <body>
 
   <div id="root">
-      
+
   </div>  
 
 </body>
--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php 
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL & ~ E_NOTICE);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,12 +14,57 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vue Dischi</title>
 
+    <!-- BOOTSTRAP -->
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
     <!-- CSS -->
 
     <link rel="stylesheet" href="css/style.css">
-    
+
 </head>
 <body>
+
+    <!-- INCLUDO IL DATABASE CON REQUIRE -->
+    <?php require_once __DIR__ . "/database.php"; ?>
+
+    <!-- HEADER -->
+
+    <header>
+
+        <div class="ms-header">
+
+            <a href="https://www.spotify.com/">
+                <img src="./img/spotify-logo.png" alt="Spotify logo" />
+            </a>
+
+        </div>
+
+    </header>
+
+    <!-- /HEADER -->
+
+    <!-- MAIN -->
+    <main>
+        <div class="container">
+            <div class="row row-cols-3 row-cols-lg-5 mt-4">
+
+                <?php foreach ($database as $element) { ?>
+
+                <div class="song-card text-center p-3 mb-3">
+                    <img class="pt-2" src="<?php echo $element["poster"]; ?>" alt="<?php echo $element["title"]; ?>"/>
+                    <h5 class="p-3"><?php echo $element["title"]; ?></h5>
+                    <span class="d-block"><?php echo $element["author"]; ?></span>
+                    <span class="d-block"><?php echo $element["year"]; ?></span>
+                </div>         
+
+                <?php } ?>
+                
+            </div>
+        </div>    
+    </main>
+
+    <!-- /MAIN -->
     
 </body>
 </html>
